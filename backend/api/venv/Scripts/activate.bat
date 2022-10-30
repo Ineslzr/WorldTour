cca563a1e98f70043438be4f3e0c9290bf9c7a1c
lazar <lazar.ines28@gmail.com>
2022-10-30 14:06:24 +0100

    ajout librairie pytest-cov

--- backend/api/venv/Scripts/activate.bat
+++ backend/api/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\lazar\Projet_M1\WorldTour\api\venv
+set VIRTUAL_ENV=C:\Users\lazar\Projet_M1\testProjet\api\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
