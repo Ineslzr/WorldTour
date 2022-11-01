0159c0c255e3cf0ce0906e355935b0a270b6f583
lazar <lazar.ines28@gmail.com>
2022-11-01 13:31:40 +0100

    Former le JSON correspondant au quiz

--- backend/api/views/quiz.py
+++ backend/api/views/quiz.py
@@ -5,5 +5,12 @@ from flask import jsonify
 @app.route("/Quiz")
 def CreateQuiz():
     nom_quiz = Quiz.getNom()
-    
-    return jsonify(nom_quiz)
+    questions = Question.getQuestions(1)
+    quiz = {"questions" : questions}
+
+    i = 0
+    while i < len(quiz["questions"]):
+        quiz["questions"][i]["choix"] = Choix.getChoix(questions[i]["id_question"])
+        i = i+1
+
+    return jsonify(quiz)
