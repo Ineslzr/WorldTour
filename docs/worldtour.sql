20c4981c383112139e1348eae78f0d5f68734c92
lazar <lazar.ines28@gmail.com>
2022-11-01 12:22:00 +0100

    Création de 3 tables dans la BD : quiz, questions et choix

--- docs/worldtour.sql
+++ docs/worldtour.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Oct 23, 2022 at 07:58 PM
+-- Generation Time: Nov 01, 2022 at 11:18 AM
 -- Server version: 5.7.24
 -- PHP Version: 8.0.1
 
@@ -20,6 +20,81 @@ SET time_zone = "+00:00";
 --
 -- Database: `worldtour`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `choix`
+--
+
+CREATE TABLE `choix` (
+  `id_choix` int(11) NOT NULL,
+  `id_question` int(11) NOT NULL,
+  `intitule` varchar(255) NOT NULL,
+  `isCorrect` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `choix`
+--
+
+INSERT INTO `choix` (`id_choix`, `id_question`, `intitule`, `isCorrect`) VALUES
+(1, 1, 'Berlin', 0),
+(2, 1, 'Paris', 1),
+(3, 1, 'Alger', 0),
+(4, 2, 'Emmanuel Macron', 1),
+(5, 2, 'Donald Trump', 0),
+(6, 2, 'Sergio Mattarella', 0),
+(7, 3, 'Notre Patrie', 0),
+(8, 3, 'La Marseillaise', 1),
+(9, 3, 'L\'Aube Nouvelle', 0),
+(10, 4, 'Livre', 0),
+(11, 4, 'Dinar', 0),
+(12, 4, 'Euro', 1),
+(13, 5, 'Français', 1),
+(14, 5, 'Anglais', 0),
+(15, 5, 'Allemand', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+
+CREATE TABLE `questions` (
+  `id_question` int(11) NOT NULL,
+  `id_quiz` int(11) NOT NULL,
+  `intitule` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `questions`
+--
+
+INSERT INTO `questions` (`id_question`, `id_quiz`, `intitule`) VALUES
+(1, 1, 'Quel est la capitale de la France ?'),
+(2, 1, 'Quel est le nom du président de la République ?'),
+(3, 1, 'Comment s\'appelle l\'hymne nationale ?'),
+(4, 1, 'Quel argent est utilisé ?'),
+(5, 1, 'Quelle langue est parlé ?');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quiz`
+--
+
+CREATE TABLE `quiz` (
+  `id_quiz` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `quiz`
+--
+
+INSERT INTO `quiz` (`id_quiz`, `nom`) VALUES
+(1, 'France - Facile');
 
 -- --------------------------------------------------------
 
@@ -48,6 +123,26 @@ INSERT INTO `users` (`id`, `name`, `email`) VALUES
 --
 
 --
+-- Indexes for table `choix`
+--
+ALTER TABLE `choix`
+  ADD PRIMARY KEY (`id_choix`),
+  ADD KEY `questions_choix` (`id_question`);
+
+--
+-- Indexes for table `questions`
+--
+ALTER TABLE `questions`
+  ADD PRIMARY KEY (`id_question`),
+  ADD KEY `quiz_questions` (`id_quiz`);
+
+--
+-- Indexes for table `quiz`
+--
+ALTER TABLE `quiz`
+  ADD PRIMARY KEY (`id_quiz`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -58,10 +153,44 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `choix`
+--
+ALTER TABLE `choix`
+  MODIFY `id_choix` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `questions`
+--
+ALTER TABLE `questions`
+  MODIFY `id_question` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `quiz`
+--
+ALTER TABLE `quiz`
+  MODIFY `id_quiz` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `choix`
+--
+ALTER TABLE `choix`
+  ADD CONSTRAINT `questions_choix` FOREIGN KEY (`id_question`) REFERENCES `questions` (`id_question`);
+
+--
+-- Constraints for table `questions`
+--
+ALTER TABLE `questions`
+  ADD CONSTRAINT `quiz_questions` FOREIGN KEY (`id_quiz`) REFERENCES `quiz` (`id_quiz`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
