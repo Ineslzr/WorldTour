7c676bbd873f3cecdd87ee8c3382678fd072573d
lazar <lazar.ines28@gmail.com>
2022-11-23 00:52:06 +0100

    Feature questionnaire

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,29 @@
-import React, {useState, useEffect} from 'react';
-
+import React from 'react';
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Link
+  } from "react-router-dom";
+import ChoixPays from './components/FiltrePays/ChoixPays';
+import Questionnaire from './components/Questionnaire/questionnaire';
+import Quiz from './components/Quiz/Quiz';
 
 function App(){
 	return(
-        <div className='application'>
-		</div>
+		<>
+		<Router>
+			<Routes>
+				<Route path="/Questionnaire" element={<Questionnaire/>}/>
+				<Route path="/ChoixPays" element={<ChoixPays/>}/>
+				<Route path="/Quiz" element={<Quiz/>}/>
+			</Routes>
+		</Router>	
+
+		<a href="/Quiz" class="active">Quiz</a> <br/>
+		<a href="/ChoixPays" class="active">Choix du pays</a><br/>
+		<a href="/Questionnaire" class="active">Questionnaire</a><br/>
+	</>
 	)  
 }
 
