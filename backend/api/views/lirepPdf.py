1c5bef873066f156946e6f87beb616a2f457c236
marwaouahouah1 <84792182+marwaouahouah1@users.noreply.github.com>
2022-11-27 19:12:19 +0100

    meilleur affichage users (mot trouvé)

--- backend/api/views/lirepPdf.py
+++ backend/api/views/lirepPdf.py
@@ -3,6 +3,7 @@ from api import app, mysql
 from api.models import *
 from flask import jsonify, request
 
+
 @app.route("/readPdf", methods=["POST"],strict_slashes=False)
 def test_pdf():
     type=request.form['option_from_react']
@@ -11,14 +12,28 @@ def test_pdf():
     page1=reader.getPage(0)
     pageExtracted=page1.extractText()
     key1='réservation'
+    key11='départ'
+    key12='aéroport'
+    key13='vol'
     key2='Exposition'
+    key21='heure'
+    key22='lieu'
+    key23='musée'
     if(type=='Avion'):
-        if((type in pageExtracted)or(key1 in pageExtracted)):
-             return jsonify(10)
+        if((type in pageExtracted)or(key1 in pageExtracted)and(key11 in pageExtracted)or(key12 in pageExtracted)or(key13 in pageExtracted)):
+              return jsonify(
+                typefichier= type,
+                key=[key1,key11,key12,key13],
+                score=10
+             )
         else: return jsonify("erreur1")
     elif(type=='Exposition'):
-        if((type in pageExtracted)or(key2 in pageExtracted)):
-             return jsonify(3)
+        if((type in pageExtracted)or(key2 in pageExtracted)or(key21 in pageExtracted)or(key22 in pageExtracted)or(key23 in pageExtracted)):
+              return jsonify(
+                typefichier= type,
+                key=[key2,key21,key22,key23],
+                score=3
+             )
         else: return jsonify("erreur2")
     else: return jsonify("erreur")
 
