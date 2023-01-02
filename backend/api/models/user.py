fde15331afdd095447d56621d7e1d97a29b7fe0e
elodie-saintaubin <backup.esa00@gmail.com>
2023-01-02 19:29:47 +0100

    Envoi d'un mail de confirmation lors de l'inscription de l'utilisateur

--- backend/api/models/user.py
+++ backend/api/models/user.py
@@ -1,8 +1,12 @@
 from api import app,mysql
 
 class User():
-    def __init__(self):
-        return 
+
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+
 
     @staticmethod
     def getAllUsers():
@@ -12,3 +16,18 @@ class User():
         cur.close()
 
         return fetchdata
+
+
+    @staticmethod
+    def addUser(name, email, password):
+        # Créez un curseur pour exécuter des requêtes SQL
+        cur = mysql.connection.cursor()
+
+        # Exécutez une requête d'insertion pour ajouter un nouvel utilisateur à la base de données
+        cur.execute("INSERT INTO user (name, email, password) VALUES (%s, %s, %s)", (name, email, password))
+
+        # Commit les changements à la base de données
+        mysql.connection.commit()
+
+        # Fermez le curseur
+        cur.close()
--- backend/api/views/user.py
+++ backend/api/views/user.py
@@ -1,9 +1,26 @@
 from api import app,mysql
 from api.models import *
-from flask import jsonify
+from flask import request, jsonify
 
 
 @app.route("/Users")
 def Users():  
     users = User.getAllUsers()
     return jsonify(users)
+
+
+@app.route("/SignUpUser", methods=["POST"])
+def signup():
+    # Récupérez les données du formulaire
+    data = request.get_json()
+    name = data["name"]
+    email = data["email"]
+    password = data["password"]
+
+    # Créez un nouvel utilisateur en utilisant les données du formulaire
+    new_user = User(name, email, password)
+
+    # Enregistrez l'utilisateur dans la base de données
+    User.addUser(name, email, password)
+
+    return jsonify({"message": "Utilisateur créé avec succès"})
